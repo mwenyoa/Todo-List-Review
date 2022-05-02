45c47d49aecbd8e1fe3ec29f81e03f82a557fd60
Anthony Mwenyo <mwenyoanthony@gmail.com>
2022-05-02 17:26:53 +0200

    Add todo list files for peer to code review and feadback

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import './style.css';
+import Tasker from './tasks.js';
+
+// tasker object
+const todoList = new Tasker();
+todoList.populateTodo();
+// Add new Activity
+document.querySelector('#add-task').addEventListener('submit', (e) => {
+  e.preventDefault();
+  const action = e.target.elements.activity.value;
+  todoList.add(action);
+  e.target.reset();
+});
+
+// clear completed Activities
+document.querySelector('.clear-completed').addEventListener('click', () => {
+  todoList.clearCompleted();
+});
+
+// Refresh window
+document.querySelector('#refresh-all').addEventListener('click', () => {
+  window.location.reload();
+});
